7b17859dbefb37a1202050dcfb74479216614e49
damon <blurm@126.com>
2017-09-04 19:56:52 +0800

    Place logo img into separate div

--- main.js
+++ main.js
@@ -41,46 +41,6 @@ function recommendResolve(dataSet) {
     }
 }
 
-function attachRatingEvent() {
-    function createOptions($target) {
-        const nameStr = $.trim($target.find('a').text()) ||
-            $.trim($target.find('a').attr('alt'));
-
-        const nameYear = TitleParser.parse(nameStr)
-        const options = {
-            name: nameYear.name,
-            year: nameYear.year,
-            type: "movie"
-        };
-        return options;
-    }
-    listHandle(createOptions, '.rarbag h3, .img');
-
-}
-function attachRatingEvent4Dytt() {
-    function createOptions($target) {
-        const nameStr = $.trim($target.find('a').text());
-
-        const nameReg = /《([^》]+)》/;
-        const yearReg = /^\d{4}/;
-        const options = {
-            name: nameStr.match(nameReg)[1],
-            year: nameStr.match(yearReg)[0],
-            type: "movie"
-        };
-        return options;
-    }
-    listHandle(createOptions, '.dytt h3');
-}
-function listHandle(createOptions, tag) {
-    const modules = [new DoubanInfo(), new IMDBInfo()];
-    const common = new Common(modules, createOptions, 'movie');
-
-    common.listHandle(
-        /.*/i,
-        tag
-    );
-}
 function reject(reason) {
     console.log(reason);
 }
@@ -166,6 +126,10 @@ document.addEventListener('DOMContentLoaded', function() {
             new CBPFWTabs( el );
         });
     })();
+    // append img logo div
+    const imgURL = chrome.extension.getURL("icon.png");
+    $('.codrops-logo').append(`<img src="${imgURL}"/>`);
+
     // 电影天堂
     new Promise((success, error) => getDytt(
         success, error))
